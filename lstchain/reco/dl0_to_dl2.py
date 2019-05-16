cb79be119c4abcf32c109317faccaa1db2af08aa
vuillaut <thomas.vuillaume@gmail.com>
2019-05-16 17:44:13 +0200

    d0 to dl2 draft to compare lstchain and gammalearn

---
+++ lstchain/reco/dl0_to_dl2.py
@@ -0,0 +1,136 @@
+from ctapipe.io import event_source
+import numpy as np
+from .dl0_to_dl1 import get_dl1
+from lstchain.reco.utils import camera_to_sky, disp_to_pos
+import os
+from lstchain.io.lstcontainers import DL1ParametersContainer
+import astropy.units as u
+import torch
+from ...utils.gammalearn import load_model, load_camera_parameters
+from ctapipe.io import HDF5TableWriter
+from ctapipe.calib import CameraCalibrator
+from ctapipe.image.extractor import NeighborPeakWindowSum
+
+
+allowed_tels = {1, 2, 3, 4}  # select LST1 only
+max_events = None  # limit the number of events to analyse in files - None if no limit
+
+threshold = 4094
+
+# Add option to use custom calibration
+
+custom = False
+
+cal = CameraCalibrator(image_extractor=NeighborPeakWindowSum())
+
+
+
+
+
+
+def r0_to_dl2(input_filename, features, e_reg, disp_reg, gh_cls, gammalearn_model, output_filename=None):
+    if output_filename is None:
+        output_filename = (
+                'dl1_' + os.path.basename(input_filename).split('.')[0] + '.h5'
+        )
+
+    source = event_source(input_filename)
+    source.allowed_tels = allowed_tels
+    source.max_events = max_events
+
+    dl1_container = DL1ParametersContainer()
+    dl1_container.prefix = ''
+
+    with HDF5TableWriter(filename=output_filename, group_name='events', overwrite=True, add_prefix=True) as writer:
+        for i, event in enumerate(source):
+            cal(event)
+            for ii, telescope_id in enumerate(event.r0.tels_with_data):
+
+                dl1_filled = get_dl1(event, telescope_id, dl1_container=dl1_container)
+                if dl1_filled is not None:
+
+                    # Some custom def
+                    dl1_container.wl = dl1_container.width / dl1_container.length
+                    # Log10(Energy) in GeV
+                    dl1_container.mc_energy = np.log10(event.mc.energy.value * 1e3)
+                    dl1_container.intensity = np.log10(dl1_container.intensity)
+                    dl1_container.gps_time = event.trig.gps_time.value
+
+                    foclen = (
+                        event.inst.subarray.tel[telescope_id]
+                            .optics.equivalent_focal_length
+                    )
+                    width = np.rad2deg(np.arctan2(dl1_container.width, foclen))
+                    length = np.rad2deg(np.arctan2(dl1_container.length, foclen))
+                    dl1_container.width = width.value
+                    dl1_container.length = length.value
+
+                    X = np.array([u.Quantity(dl1_container.as_dict()[f]).to_value() for f in features])
+
+                    e_pred = e_reg.predict(X.reshape(1, -1))
+                    disp_pred = disp_reg.predict(X.reshape(1, -1))[0]
+                    X = np.append(X, e_pred)
+                    X = np.append(X, disp_pred)
+
+                    # energy
+                    event.dl2.energy['lstchain'].energy = 10 ** (e_pred - 3) * u.TeV
+                    event.dl2.energy['lstchain'].tel_ids = {telescope_id}
+                    event.dl2.energy['lstchain'].prefix = 'lstchain'
+
+                    # direction
+                    event.dl2.shower['lstchain'].prefix = 'lstchain'
+                    palt = event.mc.tel[telescope_id].altitude_raw * u.rad
+                    paz = event.mc.tel[telescope_id].azimuth_raw * u.rad
+                    focal_length = event.inst.subarray.tel[telescope_id].optics.equivalent_focal_length
+                    src_x, src_y = disp_to_pos(disp_pred[0], disp_pred[1],
+                                               dl1_container.x.to_value(),
+                                               dl1_container.y.to_value())
+
+                    reco_altaz = camera_to_sky(src_x * u.m, src_y * u.m, focal_length, palt, paz)
+                    event.dl2.shower['lstchain'].alt = reco_altaz.alt
+                    event.dl2.shower['lstchain'].az = reco_altaz.az
+
+                    # classification
+                    event.dl2.classification['lstchain'].prefix = 'lstchain'
+                    event.dl2.classification['lstchain'].prediction = gh_cls.predict_proba(X.reshape(1, -1))[0][0]
+                    event.dl2.classification['lstchain'].tel_ids = {telescope_id}
+
+                    if width >= 0:
+                        is_lstchain_valid = True
+                    else:
+                        is_lstchain_valid = False
+
+                    event.dl2.energy['lstchain'].is_valid = is_lstchain_valid
+                    event.dl2.classification['lstchain'].is_valid = is_lstchain_valid
+                    event.dl2.shower['lstchain'].is_valid = is_lstchain_valid
+
+                    ## GammaLearn
+                    image = event.dl1.tel[telescope_id].image[0]
+                    peakpos = event.dl1.tel[telescope_id].pulse_time[0]
+
+                    data = torch.tensor([image, peakpos], dtype=torch.float).unsqueeze(0)
+                    prediction = regression_network(data).squeeze(0).detach().numpy()
+                    particle_prediction = classification_network(data)
+                    particle = torch.max(particle_prediction, 1)[1]
+
+                    event.dl2.energy['gl'].prefix = 'gl'
+                    event.dl2.shower['gl'].prefix = 'gl'
+                    event.dl2.classification['gl'].prefix = 'gl'
+                    event.dl2.energy['gl'].energy = 10 ** prediction[0] * u.TeV
+                    event.dl2.shower['gl'].core_x = prediction[1] * u.km
+                    event.dl2.shower['gl'].core_y = prediction[2] * u.km
+                    event.dl2.shower['gl'].alt = prediction[3] * u.rad
+                    event.dl2.shower['gl'].az = prediction[4] * u.rad
+                    event.dl2.classification['gl'].prediction = particle.item()
+
+                    camera = event.inst.subarray.tel[telescope_id].camera
+                    writer.write(camera.cam_id, [dl1_container,
+                                                 event.dl2.energy['lstchain'],
+                                                 event.dl2.shower['lstchain'],
+                                                 event.dl2.classification['lstchain'],
+                                                 event.dl2.energy['gl'],
+                                                 event.dl2.shower['gl'],
+                                                 event.dl2.classification['gl'],
+                                                 ])
+
+
