840ba8623091a8dc565ed4a151bea572e9ade4c0
vuillaut <thomas.vuillaume@gmail.com>
2020-01-16 17:37:06 +0100

    adding plots

--- lstchain/scripts/lstchain_mc_dl2_to_dl3_irf.py
+++ lstchain/scripts/lstchain_mc_dl2_to_dl3_irf.py
@@ -18,6 +18,11 @@ from lstchain.io.io import read_simu_info_merged_hdf5
 
 from astropy.coordinates.angle_utilities import angular_separation
 
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import ctaplot
+
+
 
 def configuration():
     config = {}
@@ -121,47 +126,8 @@ def read_and_update_dl2(filepath, tel_id=1, filters=['intensity > 300', 'leakage
     return data
 
 
-def main():
+def main(args):
 
-    parser = argparse.ArgumentParser(description='Make performance files')
-
-    parser.add_argument(
-        '--obs_time',
-        dest='obs_time',
-        type=float,
-        default=50,
-        help='Observation time in hours'
-    )
-
-    parser.add_argument('--dl2_gamma', '-g',
-                        dest='dl2_gamma_filename',
-                        type=str,
-                        required=True,
-                        help='path to the gamma dl2 file'
-                        )
-
-    parser.add_argument('--dl2_proton', '-p',
-                        dest='dl2_proton_filename',
-                        type=str,
-                        required=True,
-                        help='path to the proton dl2 file'
-                        )
-
-    parser.add_argument('--dl2_electron', '-e',
-                        dest='dl2_electron_filename',
-                        type=str,
-                        required=True,
-                        help='path to the electron dl2 file'
-                        )
-
-    parser.add_argument('--outdir', '-o',
-                        dest='outdir',
-                        type=str,
-                        default='.',
-                        help="Output directory"
-                        )
-
-    args = parser.parse_args()
     paths = {}
     paths['gamma'] = args.dl2_gamma_filename
     paths['proton'] = args.dl2_proton_filename
@@ -313,5 +279,241 @@ def main():
     sensitivity_maker.estimate_sensitivity()
 
 
+def plot_sensitivity(irf_filename, ax=None, **kwargs):
+    """
+    Plot the sensitivity
+
+    Parameters
+    ----------
+    irf_filename: path
+    ax:
+    kwargs:
+
+    Returns
+    -------
+    ax
+    """
+    ax = ctaplot.plot_sensitivity_cta_performance('north', color='black', ax=ax)
+
+    with fits.open(irf_filename) as irf:
+        t = irf['SENSITIVITY']
+        elo = t.data['ENERG_LO']
+        ehi = t.data['ENERG_HI']
+        energy = (elo + ehi)/2.
+        sens = t.data['SENSITIVITY']
+
+    if 'fmt' not in kwargs:
+        kwargs['fmt'] = 'o'
+
+    ax.errorbar(energy, sens,
+                xerr=(ehi - elo)/2.,
+                **kwargs
+                )
+
+    ax.legend(fontsize=17)
+    ax.grid(which='both')
+    return ax
+
+
+def plot_angular_resolution(irf_filename, ax=None, **kwargs):
+    """
+    Plot angular resolution from an IRF file
+
+    Parameters
+    ----------
+    irf_filename
+    ax
+    kwargs
+
+    Returns
+    -------
+
+    """
+
+    ax = ctaplot.plot_angular_resolution_cta_performance('north', color='black', ax=ax)
+
+    with fits.open(irf_filename) as irf:
+        psf_hdu = irf['POINT SPREAD FUNCTION']
+        e_lo = psf_hdu.data['ENERG_LO']
+        e_hi = psf_hdu.data['ENERG_HI']
+        energy = (e_lo + e_hi)/2.
+        psf = psf_hdu.data['PSF68']
+
+    if 'fmt' not in kwargs:
+        kwargs['fmt'] = 'o'
+
+    ax.errorbar(energy, psf,
+                xerr=(e_hi - e_lo)/2.,
+                **kwargs,
+                )
+
+    ax.legend(fontsize=17)
+    ax.grid(which='both')
+    return ax
+
+def plot_energy_resolution(gamma_filename, ax=None, **kwargs):
+    """
+    Plot angular resolution from an IRF file
+
+    Parameters
+    ----------
+    irf_filename
+    ax
+    kwargs
+
+    Returns
+    -------
+
+    """
+    data = pd.read_hdf(gamma_filename)
+
+    ax = ctaplot.plot_angular_resolution_cta_performance('north', color='black', label='CTA North', ax=ax)
+    ax = ctaplot.plot_energy_resolution(data.mc_energy, data.reco_energy, ax=ax, **kwargs)
+    ax.grid(which='both')
+    ax.set_title('Energy resoluton', fontsize=18)
+    ax.legend()
+    return ax
+
+def plot_background_rate(irf_filename, ax=None, **kwargs):
+    """
+
+    Returns
+    -------
+
+    """
+    from ctaplot.io.dataset import load_any_resource
+
+    ax = plt.gca() if ax is None else ax
+
+    bkg = load_any_resource('CTA-Performance-prod3b-v2-North-20deg-50h-BackgroundSqdeg.txt')
+    ax.loglog((bkg[0]+bkg[1])/2., bkg[2], label='CTA performances North', color='black')
+
+    with fits.open(irf_filename) as irf:
+        elo = irf['BACKGROUND'].data['ENERG_LO']
+        ehi = irf['BACKGROUND'].data['ENERG_HI']
+        energy = (elo + ehi)/2.
+        bkg = irf['BACKGROUND'].data['BGD']
+
+    if 'fmt' not in kwargs:
+        kwargs['fmt'] = 'o'
+
+    ax.errorbar(energy, bkg,
+                xerr=(ehi-elo)/2.,
+                **kwargs
+                )
+
+    ax.legend(fontsize=17)
+    ax.grid(which='both')
+    return ax
+
+
+def plot_effective_area(irf_filename, ax=None, **kwargs):
+    """
+
+    Parameters
+    ----------
+    irf_filename
+    ax
+    kwargs
+
+    Returns
+    -------
+
+    """
+
+    ax = ctaplot.plot_effective_area_cta_performance('north', color='black', ax=ax)
+
+    with fits.open(irf_filename) as irf:
+        elo = irf['SPECRESP'].data['ENERG_LO']
+        ehi = irf['SPECRESP'].data['ENERG_HI']
+        energy = (elo + ehi)/2.
+        eff_area = irf['SPECRESP'].data['SPECRESP']
+        eff_area_no_cut = irf['SPECRESP (NO CUTS)'].data['SPECRESP (NO CUTS)']
+
+    if 'label' not in kwargs:
+        kwargs['label'] = 'Effective area [m2]'
+    else:
+        user_label = kwargs['label']
+        kwargs['label'] = f'{user_label}'
+
+    ax.loglog(energy, eff_area, **kwargs)
+
+    kwargs['label'] = f"{kwargs['label']} (no cuts)"
+    kwargs['linestyle'] = '--'
+
+    ax.loglog(energy, eff_area_no_cut, **kwargs)
+
+    ax.legend(fontsize=17)
+    ax.grid(which='both')
+    return ax
+
+
+
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser(description='Make performance files')
+
+    parser.add_argument(
+        '--obs_time',
+        dest='obs_time',
+        type=float,
+        default=50,
+        help='Observation time in hours'
+    )
+
+    parser.add_argument('--dl2_gamma', '-g',
+                        dest='dl2_gamma_filename',
+                        type=str,
+                        required=True,
+                        help='path to the gamma dl2 file'
+                        )
+
+    parser.add_argument('--dl2_proton', '-p',
+                        dest='dl2_proton_filename',
+                        type=str,
+                        required=True,
+                        help='path to the proton dl2 file'
+                        )
+
+    parser.add_argument('--dl2_electron', '-e',
+                        dest='dl2_electron_filename',
+                        type=str,
+                        required=True,
+                        help='path to the electron dl2 file'
+                        )
+
+    parser.add_argument('--outdir', '-o',
+                        dest='outdir',
+                        type=str,
+                        default='.',
+                        help="Output directory"
+                        )
+
+    args = parser.parse_args()
+
+    # main(args)
+
+    irf_filename = os.path.join(args.outdir, 'irf_ThSq_r68_Time50.00h/irf.fits.gz')
+    fig_output = os.path.join(args.outdir, 'irf_ThSq_r68_Time50.00h/')
+
+    fig, ax = plt.subplots(figsize=(12, 7))
+    ax = plot_angular_resolution(irf_filename, ax=ax, label='LST1 (lstchain)')
+    fig.savefig(os.path.join(fig_output, 'angular_resolution.png'), dpi=200, fmt='png')
+
+    fig, ax = plt.subplots(figsize=(12, 7))
+    ax = plot_background_rate(irf_filename, ax=ax, label='LST1 (lstchain)')
+    fig.savefig(os.path.join(fig_output, 'background_rate.png'), dpi=200, fmt='png')
+
+    fig, ax = plt.subplots(figsize=(12, 7))
+    ax = plot_effective_area(irf_filename, ax=ax, label='LST1 (lstchain)')
+    fig.savefig(os.path.join(fig_output, 'effective_area.png'), dpi=200, fmt='png')
+
+    fig, ax = plt.subplots(figsize=(12, 7))
+    ax = plot_sensitivity(irf_filename, ax=ax, label='LST1 (lstchain)')
+    fig.savefig(os.path.join(fig_output, 'sensitivity.png'), dpi=200, fmt='png')
+
+    gamma_filename = os.path.join(args.outdir, 'irf_ThSq_r68_Time50.00h/gamma_processed.h5')
+    fig, ax = plt.subplots(figsize=(12, 7))
+    ax = plot_energy_resolution(gamma_filename, ax=ax, label='LST1 (lstchain)')
+    fig.savefig(os.path.join(fig_output, 'energy_resolution.png'), dpi=200, fmt='png')
+
