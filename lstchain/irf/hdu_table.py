f90c147a8cce2117164ad1fdfc232ccf78b73c27
vuillaut <thomas.vuillaume@gmail.com>
2020-12-07 15:25:35 +0100

    fix coord pointing

--- lstchain/irf/hdu_table.py
+++ lstchain/irf/hdu_table.py
@@ -228,7 +228,8 @@ def create_event_list(data, run_number, source_name, mode):
     pointing_az = data['pointing_az']
 
     horizon_frame = AltAz(location=location, obstime=time)
-    coord_pointing = SkyCoord(alt=pointing_alt, az=pointing_az, frame=horizon_frame)
+    frame_first_pointing = AltAz(location=location, obs_time=time[0])
+    coord_pointing = SkyCoord(alt=pointing_alt[0], az=pointing_az[0], frame=frame_first_pointing)
     coord = SkyCoord(alt=data['reco_alt'], az=data['reco_az'], frame=horizon_frame)
 
     object_radec = SkyCoord.from_name(source_name)
